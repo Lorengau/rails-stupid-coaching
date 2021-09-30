e06e5bd799e99fe5bf12356811ac9625c0993be4
Martin Krause <findmemart@gmail.com>
2021-09-30 20:45:41 +0200

    challenge_2

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,12 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    if params["question"] == "hello"
+      @answer = "hi there"
+    elsif params["question"] == "what time is it?"
+      @answer = "#{Time.now}"
+    else
+      @answer = "Please repeat your question"
+    end
   end
 end
